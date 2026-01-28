261bf48a77db4b41cb6a96ffc29190c2b987da6c
Sambhav <sambhav12wakhariya@gmail.com>
2026-01-28 20:00:45 +0530

    taking choice as an input

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ print("2. Read files and folders")
 print("3. Update the folder")
 print("4. Delete the folder")
 
+choice = int(input("Please enter your option: "))
+
+if choice == 1:
+    print("Choice 01")
+elif choice == 2:
+    print("Choice 02")
+elif choice == 3:
+    print("Choice 03")
+elif choice == 4:
+    print("Choice 04")
+else :
+    print("Invalid Choice!!!\nPlease enter valid number!\nExample : 1")
